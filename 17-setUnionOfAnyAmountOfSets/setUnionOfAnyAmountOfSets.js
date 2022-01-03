50e94085dff2743fc000b159ab27a0897a38921c
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 17:39:29 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  var result = new Set();  
+  for (let set of [...args]){
+    for (let i of set){
+      result.add(i);
+    }
+  }
+  return result;
 }
 
 // Do not edit this line;
