863bb0f9057e0f9e6c73778a557f0b1b9dfebb72
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 16:33:12 -0500

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  let cnt = 0;
+  for (let i of arr){
+    if(typeof i === "boolean" || typeof i === "string"){
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 // Do not edit this line;
