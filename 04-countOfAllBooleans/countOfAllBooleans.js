cafc91c1eeeafdec6bf2697e6d8caf2d578e776c
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 16:31:18 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let cnt = 0;
+  for (let i of arr){
+    if(typeof i === "boolean"){
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 // Do not edit this line;
