8ef17812d7d0da04d1cc7f17d8cb6e9d3024d538
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 16:15:26 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,6 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  return title.split(' ').map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 }
 
 // Do not edit this line;
