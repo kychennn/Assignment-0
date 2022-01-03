545592d9e97bff99873f58094b824153481fab18
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 17:19:50 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,13 @@
 function isPalindrome(word) {
   // Insert code here;
-  
+  if (word.length==1){
+    return true;
+  }
+  for(let i = 0; i < word.length/2; i++){
+    if(word[i] != word[word.length-1-i])
+      return false;
+  }
+  return true;
 }
 
 // Do not edit this line;
