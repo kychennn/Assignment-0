33024a9b3a3cc3933843046f1406f919cab52737
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 16:35:51 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  let cnt = 0;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i]<target){
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 // Do not edit this line;
