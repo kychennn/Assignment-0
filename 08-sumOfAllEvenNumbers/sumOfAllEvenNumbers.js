e732079f03c6cbca707c5c08a7abf23d851a8814
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 16:41:04 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  let cnt = 0;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 0){
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 // Do not edit this line;
