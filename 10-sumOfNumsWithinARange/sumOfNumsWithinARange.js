58393c348ac7e201f2a2c0c0f204c51d37eb099f
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 16:43:49 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let cnt = 0;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i]>=start && nums[i]<=end){
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 // Do not edit this line;
