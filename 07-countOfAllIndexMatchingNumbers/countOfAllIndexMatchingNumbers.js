bf4d46ae86e7c5ea90cbfa2d164e90fb70510e6e
kychennn <81440356+kychennn@users.noreply.github.com>
2022-01-03 16:39:27 -0500

    Update countOfAllIndexMatchingNumbers.js

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,12 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  let cnt = 0;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] == i){
+      cnt++;
+    }
+  }
+  return cnt;
 }
 
 // Do not edit this line;
